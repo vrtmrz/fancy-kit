8b50e64defdaa17f462118fe4fbdfa27b315b1dd
vorotamoroz <vrtmrz@gmail.com>
2026-09-04 03:30:47 +0000

    release: prepare octagonal-wheels 0.1.54

--- packages/octagonal-wheels/dist/index.js
+++ packages/octagonal-wheels/dist/index.js
@@ -56,7 +56,7 @@ export { browser_index as browser };
  * @returns The version of the library.
  */
 function libVersion() {
-    return "octagonal-wheels-0.1.53";
+    return "octagonal-wheels-0.1.54";
 }
 
 export { libVersion };
